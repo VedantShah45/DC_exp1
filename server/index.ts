74643359f1fb5cd13153a7399f60aef42f52df81
Vedant Shah <119508643+VedantShah45@users.noreply.github.com>
2025-01-30 01:20:10 +0530

    feat: Typedef for node added

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,10 @@
 // Import required modules
 import express from 'express';
-import cors from 'cors'; // Import cors
+import cors from 'cors'; 
+import dotenv from "dotenv"
+
+dotenv.config()
+
 const app = express();
 const port = process.env.PORT || 3000;
 
